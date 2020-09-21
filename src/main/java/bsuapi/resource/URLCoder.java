19fd4883583e7bdb5c9cc7c63b704d67ca6c55c7
Tony <tony.chiboucas@downstream.com>
2020-09-21 12:46:51 -0700

    [WIP] 1.3.3 Workaround attempt 2 Jersey router bug `folder/{guid}` 404

--- src/main/java/bsuapi/resource/URLCoder.java
+++ src/main/java/bsuapi/resource/URLCoder.java
@@ -1,10 +1,16 @@
 package bsuapi.resource;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class URLCoder
 {
     public static String encode(String value)
     {
         try {
+            // there's something odd going on with requests in production. Nginx decoding before reverse proxy?
+            if (StringUtils.substring(value, 0, 4).equals("http")) {
+                value = java.net.URLEncoder.encode(value, "UTF-8");
+            }
             return java.net.URLEncoder.encode(value, "UTF-8");
         } catch (Exception e) {
             return "This should not be possible.";
@@ -14,6 +20,9 @@ public class URLCoder
     public static String decode(String value)
     {
         try {
+            if (value.indexOf("%") > 0 || StringUtils.substring(value, 0, 4).equals("http")) {
+                value = java.net.URLDecoder.decode(value, "UTF-8");
+            }
             return java.net.URLDecoder.decode(value, "UTF-8");
         } catch (Exception e) {
             return "This should not be possible.";
